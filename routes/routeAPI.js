03749e64a3ff5adc050a2d15b3955b55e01001d0
maddycalvo1 <madison.calvo1@gmail.com>
2021-02-25 09:54:15 -0600

    added routes

---
+++ routes/routeAPI.js
@@ -0,0 +1,50 @@
+var db = require('../models');
+
+module.exports = function (app) {
+
+    app.get("/api/workouts", (req, res) => {
+
+        db.Workout.aggregate([
+            {
+                $addFields: {
+                    totalDuration: { $sum: "$exercises.duration" }
+                }
+            }]).then((data) => {
+                res.json(data);
+            }).catch((error) => {
+                res.sendStatus(500).json(error);
+            });
+    });
+
+    app.post("/api/workouts", (req, res) => {
+        db.Workout.create({}
+            , (err, data) => {
+                if (err) return err;
+                else res.json(data);
+            })
+
+    });
+
+    app.put("/api/workouts/:id", (req, res) => {
+        db.Workout.findByIdAndUpdate(req.params.id, { $push: { exercises: req.body } }, (err, data) => {
+            if (err) return err;
+            else res.json(data);
+        })
+    });
+
+    app.get("/api/workouts/range", (req, res) => {
+        db.Workout.aggregate([
+            {
+                $addFields: {
+                    totalDuration: { $sum: "$exercises.duration" }
+                }
+            },
+            { $limit: 7 }
+        ]).then((data) => {
+            res.json(data);
+        }).catch((error) => {
+            res.sendStatus(500).json(error);
+        });
+    });
+
+};
